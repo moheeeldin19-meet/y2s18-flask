459342916b71c67528072d8e71be10620ce8ab68
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 18:12:00 +0300

    message

--- exercises/app.py
+++ exercises/app.py
@@ -3,8 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
+    games=["skyrim","dark souls","the witcher","The legend of zelda","call of duty","life is strange","injustice","tekken","goat simulator","binding of isac","forza","mario bros","assassians creed","overwatch","portal","god of war","halo","cuphead","fallout","anime games","rocket league","guitar hero","far cry","mortal kombat","watch_dogs"]
     return render_template(
-    	"index.html")
-
+        "index.html",
+        games=games,
+        no_games=False)
+        
 if __name__ == '__main__':
    app.run(debug = True)
